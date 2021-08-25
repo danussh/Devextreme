c29951dc76ee025bf6c59d1591fb3b2dd400c003
danussh <pdanussh1997@gmail.com>
2021-08-25 16:59:08 +0530

    Todocahnges

--- src/App.js
+++ src/App.js
@@ -11,35 +11,7 @@ import Setting from './components/Dashboard.js/Sidenavcomp/Setting';
 function App() {
   const [Loggedin, setLoggedin] = useState('');
 
-  // const [change, setchange] = useState("")
-  // const [arr, setarr] = useState([])
-
-  // const clickme = ()=>{
-  //   setarr([...arr,change])
-  //   setchange("")
-  // }
-  // const deletedata = (index)=>{
-  //   console.log(index);
-  //   setarr(arr.filter((val,i)=>{
-  //     return i!==index
-  //   }))
-  // }
-  // const editdata = (index)=>{
-  //   var data = prompt("enter data to be edited")
-  //   setarr( arr.map((val,i)=>{
-  //     return i===index?data:val
-  //   }))
-  // }
   return (
-    // <>
-    // <input type="text" onChange={(e)=>setchange(e.target.value)} value={change}/>
-    // <button onClick={clickme}>Click me</button>
-    // {
-    //   arr.map((val,i)=>{
-    //     return <li key={i}>{val}<button onClick={()=>deletedata(i)}>Delete</button><button onClick={()=>editdata(i)}>edit</button></li>
-    //   })
-    // }
-    // </>
     <LoginContext.Provider value={{ Loggedin, setLoggedin }}>
       <Switch>
         <Route exact path='/' component={Login} />
